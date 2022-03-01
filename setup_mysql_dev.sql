9dfca7090635714d1ccc815ea7f48c5e0dcc5c81
root <3626@holbertonschool.com>
2022-03-01 12:54:49 -0800

    3 and 4

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,1 +1,13 @@
-test
+-- prepairing MySQL for the project using database hbnb_dev_db
+-- a new user in localhost
+-- password is password jk its hbnb_dev_pwd
+-- hbnb_dev has all privileges on the db
+-- hbnb_dev has select privileges on performance_schema
+-- if hbnb_dev_db or user hbnb_dev already exists script should'nt fail
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS "hbnb_dev"@"localhost";
+CREATE USER "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO "hbnb_dev"@"localhost";
+GRANT SELECT ON `performance_schema`.* TO "hbnb_dev"@"localhost";
+FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,1 +1,13 @@
-test
+-- Prepairing MySQL for the project
+-- database of hbnb_test_db a new user hbnb_test in localhost
+-- password of hbnb_test set to hbnb_test_pwd
+-- hbnb_test gets all privileges on hbnb_test_db
+-- hbnb_test has select privileges on performance_schema
+-- if hbnb_test or hbnb_test exists script shouldn't fail
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+DROP USER IF EXISTS "hbnb_test"@"localhost";
+CREATE USER "hbnb_test"@"localhost" IDENTIFIED BY "hbnb_test_pwd";
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO "hbnb_test"@"localhost";
+GRANT SELECT ON `performance_schema`.* TO "hbnb_test"@"localhost";
+FLUSH PRIVILEGES;
