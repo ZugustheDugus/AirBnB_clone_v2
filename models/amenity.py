51c2704958f112c817340fc10c3cd2eb9b12a063
root <3626@holbertonschool.com>
2022-03-02 10:01:47 -0800

    test

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Table, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Amenity(BaseModel):
-    name = ""
+place_amenity = Table("place_amenity", Base.metadata, Column
+                        ("place_id", String(60), ForeignKey
+                        ("places.id"), primary_key=True,
+                        nullable=False), Column("amenity_id",
+                        String(60), ForeignKey("amenities.id"),
+                        primary_key=True, nullable=False))
+class Amenity(BaseModel, Base):
+    """
+    name: Amenities name
+    Place_amenities(relationship): Place = Amenitity relationship
+    """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity)
+    else:
+        name = ""
