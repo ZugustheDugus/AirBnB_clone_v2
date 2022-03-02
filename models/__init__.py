0f428dbb1520967e4da64adc59c184879075446a
root <3626@holbertonschool.com>
2022-03-02 12:03:42 -0800

    test

--- models/__init__.py
+++ models/__init__.py
@@ -10,10 +10,10 @@ from models.review import Review
 from models.amenity import Amenity
 from models.base_model import BaseModel
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        from models.engine.db_storage import DBStorage
-        storage = DBStorage()
-    else:
-        from models.engine.file_storage import FileStorage
-        storage = FileStorage()
-    storage.reload()
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
