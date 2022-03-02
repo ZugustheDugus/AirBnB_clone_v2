33c7823d1794e58f7963048b75a1e214a7960119
root <3626@holbertonschool.com>
2022-03-02 13:43:46 -0800

    test

--- models/__init__.py
+++ models/__init__.py
@@ -2,15 +2,10 @@
 """This module instantiates an object of class FileStorage"""
 
 from os import getenv
-from models.user import User
-from models.city import City
-from models.state import State
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
-from models.base_model import BaseModel
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
--- models/user.py
+++ models/user.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 import models
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship('Place', backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", casecade="delete")
+    if models.storage_t == 'db':
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship('Place', backref="user", cascade="delete")
+        reviews = relationship("Review", backref="user", casecade="delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
         """
