247e515d9069fbc95d39ea386be32d68dcc043f7
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-03-20 23:42:55 -0500

    Asdf

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """
-Script that starts a flask web application
+Script to start a Flask web application
+web app must be listening on 0.0.0.0:5000
+/: will display "Hello HBNB!"
+sctrict_slashes=False required in route
 """
+
+
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """
-    Displays Hello HBNB!
-    """
+@app.route("/", strict_slashes=False)
+def hello():
+    """displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
-if __name__ == '__main__':
-    app.run('0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
